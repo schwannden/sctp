6ca280a166479b915d1c7367c9352a02ea463531
schwannden <schwannden@gmail.com>
2015-04-09 17:19:00 +0800

    README update

--- README.md
+++ README.md
@@ -2,8 +2,21 @@
 
 ## Pre-requisite
 
+#### First we need sctp
+
 * Fedora
-sudo yum install lksctp\*
+``sudo yum install lksctp\*``
 
 * Debian/Ubuntu
-sudo apt-get install libsctp-dev lksctp-tools
+``sudo apt-get install libsctp-dev lksctp-tools``
+
+#### Second we need libsnp
+git clone https://github.com/schwannden/libsnp
+cd libsnp
+make
+sudo make install
+
+For those system whose ``LD_LIBRARY_PATH does not contain /usr/lib
+and libsctp or libsnp is installed in /usr/lib
+
+``source environment-setup``
--- sctp_client.c
+++ sctp_client.c
@@ -5,8 +5,8 @@
 #define SERV_MAX_SCTP_STRM 10
 #endif
 
-void sctpstr_cli(FILE *fp, int sock_fd, struct sockaddr *to, socklen_t tolen);
-void sctpstr_cli_echoall(FILE *fp, int sock_fd, struct sockaddr *to, socklen_t tolen);
+void sctpstr_cli(int in_fd, int sock_fd, struct sockaddr *to, socklen_t tolen);
+void sctpstr_cli_echoall(int in_fd, int sock_fd, struct sockaddr *to, socklen_t tolen);
 void sctpSubscribeEvent (struct sctp_event_subscribe * events);
 
 int
@@ -37,58 +37,81 @@ main(int argc, char **argv)
   Setsockopt(sock_fd, IPPROTO_SCTP, SCTP_EVENTS,
        &events, sizeof(events));
   if(echo_to_all == 0)
-    sctpstr_cli (stdin, sock_fd, (SA*) &servaddr, sizeof (servaddr));
+    sctpstr_cli (STDIN_FILENO, sock_fd, (SA*) &servaddr, sizeof (servaddr));
   else
-    sctpstr_cli_echoall (stdin, sock_fd, (SA*) &servaddr, sizeof (servaddr));
+    sctpstr_cli_echoall (STDIN_FILENO, sock_fd, (SA*) &servaddr, sizeof (servaddr));
   Close (sock_fd);
   return (0);
 }
 
 void
-sctpstr_cli (FILE *fp, int sock_fd, struct sockaddr *to, socklen_t tolen)
+sctpstr_cli (int in_fd, int sock_fd, struct sockaddr *to, socklen_t tolen)
 {
   struct sockaddr_in     peeraddr;
   struct sctp_sndrcvinfo sri;
   char                   sendline[SCTP_MAXBLOCK], recvline[SCTP_MAXBLOCK];
   socklen_t              len;
-  int                    out_sz,rd_sz;
-  int                    msg_flags;
+  int                    out_sz, rd_sz, msg_flags, max_fd, eof = 0;
+  fd_set                 rset;
 
+  FD_ZERO (&rset);
   bzero (&sri, sizeof (sri));
-  while (fgets (sendline, SCTP_MAXBLOCK, fp) != NULL)
+  max_fd = max (in_fd, sock_fd) + 1;
+  while (1)
     {
-      if (sendline[0] != '[')
+      if (eof == 0)
+        FD_SET( in_fd, &rset );
+      FD_SET( sock_fd, &rset );
+      max_fd = max (in_fd, sock_fd) + 1;
+      //blocking for either user or socket inputs
+      Select( max_fd, &rset, NULL, NULL, NULL);
+
+      if( FD_ISSET(in_fd, &rset) )
         {
-          printf ("Error, line must be of the form '[streamnum]text'\n");
-          continue;
+          if ( Readline (in_fd, sendline, SCTP_MAXBLOCK) == 0 )
+            {
+              eof = 1;
+              FD_CLR (in_fd, &rset);
+            }
+          if (sendline[0] != '[')
+            {
+              printf ("Error, line must be of the form '[streamnum]text'\n");
+              continue;
+            }
+          sri.sinfo_stream = strtol (&sendline[1], NULL, 0);
+          out_sz = strlen (sendline);
+          printf ("read %d bytes from input\n", out_sz);
+          Sctp_sendmsg (sock_fd, sendline, out_sz, to, tolen, 
+                        0, 0, sri.sinfo_stream, 0, 0);
+          printf ("sent %d bytes\n", out_sz);
         }
-      sri.sinfo_stream = strtol (&sendline[1], NULL, 0);
-      out_sz = strlen (sendline);
-      printf ("read %d bytes from input\n", out_sz);
-      Sctp_sendmsg (sock_fd, sendline, out_sz, to, tolen, 
-                    0, 0, sri.sinfo_stream, 0, 0);
-      printf ("sent %d bytes\n", out_sz);
-     
-      len = sizeof (peeraddr);
-      rd_sz = Sctp_recvmsg (sock_fd, recvline, sizeof(recvline),
-                           (SA*) &peeraddr, &len,
-                           &sri,&msg_flags);
-      if (msg_flags & MSG_NOTIFICATION)
+      if( FD_ISSET(sock_fd, &rset) )
         {
-          print_notification (sock_fd, recvline);
-        }
-      else
-        {
-          printf ("From str:%d seq:%d (assoc:0x%x):",
-                  sri.sinfo_stream,sri.sinfo_ssn,
-                  (u_int)sri.sinfo_assoc_id);
-          printf ("%.*s",rd_sz,recvline);
+          len = sizeof (peeraddr);
+          printf ("Read from Sctp_recvmsg\n");
+          rd_sz = Sctp_recvmsg (sock_fd, recvline, sizeof(recvline),
+                               (SA*) &peeraddr, &len,
+                               &sri,&msg_flags);
+          printf ("Sctp_recv returns %d\n", rd_sz);
+          if (msg_flags & MSG_NOTIFICATION)
+            {
+              print_notification (sock_fd, recvline);
+              if (((union sctp_notification*) recvline)->sn_header.sn_type == SCTP_SHUTDOWN_EVENT)
+                return;
+            }
+          else
+            {
+              printf ("From str:%d seq:%d (assoc:0x%x):",
+                      sri.sinfo_stream,sri.sinfo_ssn,
+                      (u_int)sri.sinfo_assoc_id);
+              printf ("%.*s",rd_sz,recvline);
+            }
         }
     }
 }
 
 void
-sctpstr_cli_echoall (FILE *fp, int sock_fd, struct sockaddr *to, socklen_t tolen)
+sctpstr_cli_echoall (int in_fd, int sock_fd, struct sockaddr *to, socklen_t tolen)
 {
   struct sockaddr_in     peeraddr;
   struct sctp_sndrcvinfo sri;
@@ -99,29 +122,34 @@ sctpstr_cli_echoall (FILE *fp, int sock_fd, struct sockaddr *to, socklen_t tolen
 
   bzero (sendline, sizeof (sendline));
   bzero (&sri, sizeof (sri));
-  while (fgets (sendline, SCTP_MAXLINE - 9, fp) != NULL) {
-    strsz = strlen (sendline);
-    if (sendline[strsz-1] == '\n') {
-      sendline[strsz-1] = '\0';
-      strsz--;
+  while (1)
+    {
+      Readline (in_fd, sendline, SCTP_MAXLINE - 9);
+      strsz = strlen (sendline);
+      if (sendline[strsz-1] == '\n')
+        {
+          sendline[strsz-1] = '\0';
+          strsz--;
+        }
+      for (i=0 ; i<SERV_MAX_SCTP_STRM ; i++)
+        {
+          snprintf (sendline + strsz, sizeof (sendline) - strsz, ".msg.%d 1", i);
+          Sctp_sendmsg (sock_fd, sendline, strsz, 
+                        to, tolen, 0, 0, i, 0, 0);
+          snprintf (sendline + strsz, sizeof (sendline) - strsz, ".msg.%d 2", i);
+          Sctp_sendmsg (sock_fd, sendline, strsz, 
+                        to, tolen, 0, 0, i, 0, 0);
+        }
+      for (i=0 ; i<SERV_MAX_SCTP_STRM*2 ; i++)
+        {
+          len = sizeof(peeraddr);
+          rd_sz = Sctp_recvmsg (sock_fd, recvline, sizeof(recvline),
+                                (SA*) &peeraddr, &len, &sri,&msg_flags);
+          printf ("From str:%d seq:%d (assoc:0x%x):", sri.sinfo_stream,
+                   sri.sinfo_ssn, (u_int) sri.sinfo_assoc_id);
+          printf ("%.*s\n",rd_sz,recvline);
+        }
     }
-    for (i=0 ; i<SERV_MAX_SCTP_STRM ; i++) {
-      snprintf (sendline + strsz, sizeof (sendline) - strsz, ".msg.%d 1", i);
-      Sctp_sendmsg (sock_fd, sendline, strsz, 
-                    to, tolen, 0, 0, i, 0, 0);
-      snprintf (sendline + strsz, sizeof (sendline) - strsz, ".msg.%d 2", i);
-      Sctp_sendmsg (sock_fd, sendline, strsz, 
-                    to, tolen, 0, 0, i, 0, 0);
-    }
-    for (i=0 ; i<SERV_MAX_SCTP_STRM*2 ; i++) {
-      len = sizeof(peeraddr);
-      rd_sz = Sctp_recvmsg (sock_fd, recvline, sizeof(recvline),
-                            (SA*) &peeraddr, &len, &sri,&msg_flags);
-      printf ("From str:%d seq:%d (assoc:0x%x):", sri.sinfo_stream,
-               sri.sinfo_ssn, (u_int) sri.sinfo_assoc_id);
-      printf ("%.*s\n",rd_sz,recvline);
-    }
-  }
 }
 
 void
